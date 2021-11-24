8c4a064360ae4a56f1b9fc47089abf65fba389d3
Henry Zhang <henryzhang757@gmail.com>
2021-11-24 12:15:18 -0500

    Added removeFromList

--- options.js
+++ options.js
@@ -1,3 +1,4 @@
+
 chrome.storage.sync.get(['userData'], function(result) {
     whiteList = result.userData.whiteList;
     blackList = result.userData.blackList;
@@ -7,11 +8,11 @@ chrome.storage.sync.get(['userData'], function(result) {
 
     // populate the lists
     whiteList.forEach(element => {
-        whiteListElement.innerHTML += "<button id=\"whiteElement\">" + element +  " X</button>"
+        whiteListElement.innerHTML += "<button class=\"whiteElement\">" + element +  " X</button>"
     })
 
     blackList.forEach(element => {
-        blackListElement.innerHTML += "<button id=\"blackElement\">" + element + " X</button>"
+        blackListElement.innerHTML += "<button class=\"blackElement\">" + element + " X</button>"
     })
 
     var addList = function()
@@ -19,23 +20,60 @@ chrome.storage.sync.get(['userData'], function(result) {
         if (this.id == "addWhiteListButton")
         {
             whiteList.push(document.getElementById("addWhiteList").value.toLowerCase())
-            whiteListElement.innerHTML += "<button id=\"whiteElement\">" + document.getElementById("addWhiteList").value.toLowerCase() +  " X</button>"
+            whiteListElement.innerHTML += "<button class=\"whiteElement\">" + document.getElementById("addWhiteList").value.toLowerCase() +  " X</button>"
             document.getElementById("addWhiteList").value = ""
+
             userData = {whiteList: whiteList, blackList: blackList}
             chrome.storage.sync.set({userData: userData}, function() {
                 console.log('Value is set to ' + userData)
             });
+            document.getElementById("addWhiteListButton").onclick = addList;
         }
         else
         {
             blackList.push(document.getElementById("addBlackList").value.toLowerCase())
-            blackListElement.innerHTML += "<button id=\"blackElement\">" + document.getElementById("addBlackList").value +  " X</button>"
+            blackListElement.innerHTML += "<button class=\"blackElement\">" + document.getElementById("addBlackList").value.toLowerCase() +  " X</button>"
             document.getElementById("addBlackList").value = ""
+
+            userData = {whiteList: whiteList, blackList: blackList}
+            chrome.storage.sync.set({userData: userData}, function() {
+                console.log('Value is set to ' + userData)
+            });
+            document.getElementById("addBlackListButton").onclick = addList;
         }
-        document.getElementById("addWhiteListButton").onclick = addList;
-        document.getElementById("addBlackListButton").onclick = addList;
+    }
+
+    var removeFromList = function()
+    {
+        subredditName = this.innerText.substring(0, this.innerText.length-2);
+        //const isSubredditname = (element)
+        // find the subreddit name whiteList/blackList and remove it from there
+        // update google storage
+        // remove the element from the options page
+
+        if (this.id == "whiteElement")
+        {
+            this.remove();
+        }
+        else
+        {
+            this.remove();
+        }
     }
 
     document.getElementById("addWhiteListButton").onclick = addList;
     document.getElementById("addBlackListButton").onclick = addList;
+
+
+    let whiteElements = document.getElementsByClassName("whiteElement");
+
+    for(let i = 0; i < whiteElements.length; i++) {
+        whiteElements[i].onclick = removeFromList
+    }
+
+    let blackElements = document.getElementsByClassName("blackElement");
+
+    for(let i = 0; i < blackElements.length; i++) {
+        blackElements[i].onclick = removeFromList
+    }
 });
